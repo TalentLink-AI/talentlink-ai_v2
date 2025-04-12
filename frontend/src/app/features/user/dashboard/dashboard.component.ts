da72de3a27686f32003d6494cd0fb5d5b59bd22d
BuildTools <unconfigured@null.spigotmc.org>
2025-04-11 23:47:21 -0400

    Jobs

--- frontend/src/app/features/user/dashboard/dashboard.component.ts
+++ frontend/src/app/features/user/dashboard/dashboard.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+//Services
+import { UserService } from '../../../services/user.service';
+//Components
 import { ProfileComponent } from '../../profile/profile.component';
+import { JobListingComponent } from '../../jobs/job-listing/job-listing.component';
 
 @Component({
   selector: 'app-user-dashboard',
   templateUrl: './dashboard.component.html',
-  imports: [CommonModule, ProfileComponent],
+  imports: [CommonModule, ProfileComponent, JobListingComponent],
   styleUrls: ['./dashboard.component.scss'],
 })
 export class UserDashboardComponent {
   activeTab: string = 'jobs';
+  userRole: any;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUserRoles().subscribe((roles) => {
+      console.log('User roles:', roles);
+      if (roles.includes('client')) {
+        this.userRole = 'client';
+      } else if (roles.includes('talent')) {
+        this.userRole = 'talent';
+      } else {
+        this.userRole = 'guest'; // fallback
+      }
+    });
+  }
 
   setTab(tab: string) {
     this.activeTab = tab;
